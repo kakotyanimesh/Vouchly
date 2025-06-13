f42d924c82f46adbe97fc5d1c5e387e38154c020
animesh <phb21013@tezu.ac.in>
2025-06-13 19:45:56 +0530

    whole ui changed

--- src/app/(user)/space/createForm/page.tsx
+++ src/app/(user)/space/createForm/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { FormDiv } from "@/components/formDiv";
 import { PreviewForm } from "@/components/previewform";
+import { Suspense } from "react";
 
 
 
@@ -11,7 +12,9 @@ export default function TestimoniaForm() {
     return (
         <div className="md:-mx-16 -mx-3">
             <div className="flex md:flex-row flex-col gap-5 md:mx-10 mx-2 justify-between">
-                <FormDiv/>
+                <Suspense fallback={<h1>loading ....</h1>}>
+                    <FormDiv/>
+                </Suspense>
                 <PreviewForm/>
             </div>
         </div>
