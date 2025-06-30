62162f1b9c13149ff4567b900dd3f6eb367c751c
animesh <phb21013@tezu.ac.in>
2025-06-30 09:22:37 +0530

    changes markdown file and created open graph and twitter banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,44 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local"
+import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "sonner";
 
-const myFont = localFont({src : "../font/localfont.otf"})
-
-
+const myFont = localFont({ src: "../font/localfont.otf" });
 
 export const metadata: Metadata = {
-  title: "Embriefy",
-  description: "CollectCustomerStories Effortlessly",
+	title: "Embriefy",
+	description: "CollectCustomerStories Effortlessly",
+	openGraph: {
+		title: "Embriefy",
+		description: "Build Testimonial Pages in minutes",
+		images: [
+			{
+				url: `${process.env.NEXT_PUBLIC_NEXT_URL}og-image.png`,
+				width: 1200,
+				height: 630,
+				alt: "Embriefy Banner",
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Embriefy",
+		description: "Build Testimonial Pages in minutes",
+		images: [`${process.env.NEXT_PUBLIC_NEXT_URL}og-image.png`],
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${myFont.className} antialiased`}
-      >
-
-        {children}
-        <Toaster position="bottom-right" richColors  />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${myFont.className} antialiased`}>
+				{children}
+				<Toaster position="bottom-right" richColors />
+			</body>
+		</html>
+	);
 }
